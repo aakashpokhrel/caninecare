f876409bbdadb60cac77ae8574c2d44c22010b0f
[Sulav Shrestha] <[Sulavstha34@gmail.com]>
2021-08-12 14:05:05 +0545

    Profile added

---
+++ app/src/main/java/com/example/canine_care/ui/ProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.example.canine_care.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.canine_care.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        //profile xcvbn
+    }
+}
