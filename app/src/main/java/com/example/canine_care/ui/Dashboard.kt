5841aae565a762d98f001548f1a47f34632a357c
Aakash Pokhrel <a.pokhrel086@gmail.com>
2021-08-17 14:00:02 +0545

    work done in admin

---
+++ app/src/main/java/com/example/canine_care/ui/Dashboard.kt
@@ -0,0 +1,50 @@
+package com.example.canine_care.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.cardview.widget.CardView
+import com.example.canine_care.R
+
+class Dashboard : AppCompatActivity() {
+
+
+
+    private lateinit var btnAddProduct: CardView
+    private lateinit var btnUpdateProduct: CardView
+    private lateinit var btnAddPet : CardView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+
+        btnUpdateProduct = findViewById(R.id.btnUpdateProduct)
+        btnAddProduct = findViewById(R.id.btnAddProduct)
+        btnAddPet = findViewById(R.id.btnAddPet)
+
+        btnAddProduct.setOnClickListener {
+            val intent = Intent(
+                this, AddProductActivity::class.java
+            )
+            startActivity(intent)
+        }
+
+        btnAddPet.setOnClickListener {
+            val intent = Intent(
+                this, AddPetActivity::class.java
+            )
+            startActivity(intent)
+        }
+
+
+
+        btnUpdateProduct.setOnClickListener {
+            val intent = Intent(
+                this, UpdateProductActivity::class.java
+            )
+            startActivity(intent)
+        }
+    }
+}
