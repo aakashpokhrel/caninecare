85af15b7b6bff8e40ef1a0b7d0d635cc8191c3b0
[Sulav Shrestha] <[Sulavstha34@gmail.com]>
2021-08-18 15:03:20 +0545

    Updated Medical activity

--- app/src/main/java/com/example/canine_care/ui/MedicalActivity.kt
+++ app/src/main/java/com/example/canine_care/ui/MedicalActivity.kt
@@ -1,12 +1,20 @@
 package com.example.canine_care.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.canine_care.R
 
 class MedicalActivity : AppCompatActivity() {
+    private lateinit var iminformation: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_medical)
+        iminformation = findViewById(R.id.iminformation)
+        iminformation.setOnClickListener {
+            val intent = Intent(this@MedicalActivity, InformationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
